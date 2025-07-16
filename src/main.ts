815114f1ec713086c77e702680e2ea9b3644baf4
nickgive <winneryep1210@gmail.com>
2025-07-16 22:31:03 +0900

    add src folder

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import {
+    Client,
+    Events,
+    GatewayIntentBits
+} from 'discord.js';
+
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
